51ca6cece07a92755be9420da8166bccfebc15d0
sidav <sidav94@gmail.com>
2022-02-08 20:37:36 +0300

    Makes color-strings IO rework.

--- console_io_colored_texts.go
+++ console_io_colored_texts.go
@@ -5,6 +5,25 @@ import (
 	"hydras2/entities"
 )
 
+func (c *consoleIO) setFgColorByColorTag(tagName string) {
+	switch tagName {
+	case "RED":
+		c.style = c.style.Foreground(tcell.ColorRed)
+	case "YELLOW":
+		c.style = c.style.Foreground(tcell.ColorYellow)
+	case "BLUE":
+		c.style = c.style.Foreground(tcell.ColorBlue)
+	case "CYAN":
+		c.style = c.style.Foreground(tcell.ColorLightCyan)
+	case "DARKGRAY":
+		c.style = c.style.Foreground(tcell.ColorDarkGray)
+	case "RESET":
+		c.resetStyle()
+	default:
+		panic("Y U NO IMPLEMENT")
+	}
+}
+
 func (c *consoleIO) putColorTaggedString(str string, x, y int) {
 	if !entities.IsStringColorTagged(str) {
 		c.putUncoloredString(str, x, y)
@@ -12,26 +31,11 @@ func (c *consoleIO) putColorTaggedString(str string, x, y int) {
 	}
 	offset := 0
 	for i := 0; i < len(str); i++ {
-		skip := true
-		switch entities.GetColorTagNameInStringAtPosition(str, i) {
-		case "RED":
-			c.setStyle(tcell.ColorRed, tcell.ColorBlack)
-		case "YELLOW":
-			c.setStyle(tcell.ColorYellow, tcell.ColorBlack)
-		case "BLUE":
-			c.setStyle(tcell.ColorBlue, tcell.ColorBlack)
-		case "CYAN":
-			c.setStyle(tcell.ColorLightCyan, tcell.ColorBlack)
-		case "DARKGRAY":
-			c.setStyle(tcell.ColorDarkGray, tcell.ColorBlack)
-		case "RESET":
-			c.resetStyle()
-		default:
-			skip = false
-		}
-		if skip {
+		tag := entities.GetColorTagNameInStringAtPosition(str, i)
+		if tag != "" {
 			i += entities.COLOR_TAG_LENGTH
 			offset += entities.COLOR_TAG_LENGTH
+			c.setFgColorByColorTag(tag)
 		}
 		c.screen.SetCell(x+i-offset, y, c.style, rune(str[i]))
 	}
